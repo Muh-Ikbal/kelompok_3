062a291c64a289acfd5833728affc4aeb95a63e3
Mursawal <mursyawal29@gmail.com>
2023-11-23 20:00:38 +0800

    jadwal from sawal

---
+++ admin/proses.php
@@ -0,0 +1,52 @@
+<?php
+    include "koneksikapal.php";
+
+    if(isset($_POST['btnProses'])){
+        $nama_kapal = $_POST["nama_kapal"];
+        $muatan = $_POST["muatan"];
+        $tujuan = $_POST["tujuan"];
+        $harga = $_POST["harga"];
+
+        if($_POST['btnProses']=='tambah'){
+        $gambar = $_FILES["gambar"]["name"];
+        $gambar_tmp = $_FILES["gambar"]["tmp_name"];
+        move_uploaded_file($gambar_tmp,"gambar/".$gambar);
+        
+        $query="INSERT INTO tb_jadwal (`id_kapal`, `nama_kapal`, `muatan`, `tujuan`, `harga`, `gambar`) 
+        VALUES (NULL, '$nama_kapal', '$muatan', '$tujuan', '$harga', '$gambar')";
+        $sql=mysqli_query($konek,$query);
+
+        if($sql){
+            header("location:jadwal.php");
+        }
+        }else{
+            if($_FILES['gambar']['name']!=""){
+                $query=mysqli_query( $konek,"SELECT * FROM tb_jadwal WHERE `id_kapal` = '$_POST[id]'");
+                $data=mysqli_fetch_array($query);
+                unlink("gambar/". $data["name"]);
+
+                $gambar = $_FILES["gambar"]["name"];
+                $gambar_tmp = $_FILES["gambar"]["tmp_name"];
+                move_uploaded_file($gambar_tmp,"gambar/".$gambar);
+
+                mysqli_query( $konek,"UPDATE `tb_jadwal` SET nama_kapal = '$nama_kapal', muatan = '$muatan', tujuan = '$tujuan', `harga` = '$harga', gambar = '$gambar' WHERE `id_kapal` = '$_POST[id_kapal]'");
+                header("location:jadwal.php");
+            }else{
+                mysqli_query( $konek,"UPDATE `tb_jadwal` SET nama_kapal = '$nama_kapal', muatan = '$muatan', tujuan = '$tujuan', `harga` = '$harga' WHERE `id_kapal` = '$_POST[id_kapal]'");
+                header("location:jadwal.php");
+            }
+        }
+    }elseif(isset($_GET["hapus"])){
+        $querihapus="SELECT gambar FROM tb_jadwal WHERE `id_kapal` = $_GET[hapus]";
+        $sqlhapus=mysqli_query($konek,$querihapus);
+        $data=mysqli_fetch_array($sqlhapus);
+        unlink("gambar/". $data["gambar"]);
+    
+        $query="DELETE FROM tb_jadwal WHERE id_kapal = $_GET[hapus]";
+        $sql=mysqli_query($konek,$query);
+        if($sql){
+            header("location:jadwal.php");
+        }
+    }
+
+?>
