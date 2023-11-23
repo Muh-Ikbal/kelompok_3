43a5a6a1978672cc986f44d11b37c9d8db395e9b
tyrant04 <ziqriafdal95@gmail.com>
2023-11-23 17:49:42 +0800

    ubah lagi

--- index.php
+++ index.php
@@ -23,32 +23,12 @@ include("koneksi.php");
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-          <div class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-person-circle" viewBox="0 0 16 16">
-                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                <path fill-rule="evenodd"
-                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-              </svg>
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Log Out</a></li>
-              <li>
-                <hr class="dropdown-divider" />
-              </li>
-              <li class="dropdown-item">User 'afdal</li>
-            </ul>
-          </div>
-        </div>
       </div>
     </nav>
 
     <div class="overlay">
       <h1>
-        <img src="../images/Modern Initial MB Logo.png" alt="logo Aplikasi" class="img-responsive"
-          style="width: 200px;" />
+        <img src="images/Modern Initial MB Logo.png" alt="logo Aplikasi" class="img-responsive" style="width: 200px;" />
       </h1>
       <h1>
         Selamat Datang di
@@ -57,7 +37,7 @@ include("koneksi.php");
       <p style="font-family: Brush Script Std; font-size: 21px">
         Pesan tiket hanya dengan 1 klik
       </p>
-      <a href="dashboard.php"><button type="button" class="btn btn-primary">Cari Tiket</button></a>
+      <a href="tiket/index.php"><button type="button" class="btn btn-primary">Cari Tiket</button></a>
     </div>
 
   </section>
